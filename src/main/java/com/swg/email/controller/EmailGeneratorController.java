35cf2824e407dcd729ec9d5eaed27f5b0b5acf6a
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-02-06 14:52:39 +0530

    feat : minor changes

--- src/main/java/com/swg/email/controller/EmailGeneratorController.java
+++ src/main/java/com/swg/email/controller/EmailGeneratorController.java
@@ -1,17 +1,30 @@
 package com.swg.email.controller;
 
 import com.swg.email.dto.EmailRequest;
+import com.swg.email.service.EmailGeneratorService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+//http://localhost:8080/api/email/generate
+
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/email")
 public class EmailGeneratorController {
 
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
-        return ResponseEntity.ok("Email generated successfully for: ");
+
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+
+        return ResponseEntity.ok(response);
     }
 
 }
