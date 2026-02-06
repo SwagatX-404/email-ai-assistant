acbeb7b99636c04bfbf3c65a29d81f2c0abe34b2
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-02-06 13:03:18 +0530

    fix: main

---
+++ src/main/java/com/swg/email/EmailSbApplication.java
@@ -0,0 +1,13 @@
+package com.swg.email;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSbApplication {
+
+    public static void main (String[] args){
+        SpringApplication.run(EmailSbApplication.class, args);
+    }
+}
