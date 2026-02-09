913afe9c22426299e28951a1e95d8f79954e1dbf
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-02-09 15:03:22 +0530

    fix: cors policy add @CrossOrigin(origins ="*")

--- src/main/java/com/swg/email/controller/EmailGeneratorController.java
+++ src/main/java/com/swg/email/controller/EmailGeneratorController.java
@@ -4,10 +4,7 @@ import com.swg.email.dto.EmailRequest;
 import com.swg.email.service.EmailGeneratorService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 //http://localhost:8080/api/email/generate
@@ -15,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/email")
+@CrossOrigin(origins ="*")
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
